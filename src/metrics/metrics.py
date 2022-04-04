53416131803f9fa7956ab3f09d4a5bf9f7c3afe9
Nikita Stepanov <stepanov2000nikita@yandex.ru>
2022-04-04 19:13:28 +0300

    fix bug in mAP calculation

--- src/metrics/metrics.py
+++ src/metrics/metrics.py
@@ -10,6 +10,6 @@ def mAP(logits, targets):
     targets = targets.gather(dim=1, index=torch.argsort(logits, dim=-1,
                                                         descending=True))
     ap = (targets * torch.cumsum(targets, dim=-1) /\
-        torch.arange(1, targets.shape[-1] + 1)).sum(dim=-1) /\
-        targets.sum(dim=-1)
+        torch.arange(1, targets.shape[-1] + 1, device=targets.device))\
+        .sum(dim=-1) / targets.sum(dim=-1)
     return ap.mean()
