e312e9f6ac56065ef2ef92507fd8636ac592efd1
Nikita Stepanov <stepanov2000nikita@yandex.ru>
2022-04-04 18:41:27 +0300

    add passt finetuning on ssp

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,4 +1,8 @@
+from .multi_label_clsf_collator import MultiLabelClassificationCollator
 from .SSPNetVC import SSPNetVC
 
 
-__all__ = ['SSPNetVC']
+__all__ = [
+    'MultiLabelClassificationCollator',
+    'SSPNetVC'
+]
